b67b83e5b376b105d0b166543f8d782b3875d84d
anders462 <anders462@gmail.com>
2016-02-08 13:37:58 -0800

    changed settings

--- Voting/public/scripts/pollfactory.js
+++ Voting/public/scripts/pollfactory.js
@@ -1,5 +1,6 @@
 
 
+
 (function(){
 
 'use strict';
@@ -8,7 +9,7 @@ angular.module('votingApp')
 .constant("baseUrl", "https://anders-voteup.herokuapp.com/api/poll")
 .service('PollFactory', ['baseUrl', '$http', function(baseUrl, $http) {
 
-//https://anders-voteup.herokuapp.com/api/poll
+
 this.addPoll = function(newPoll) {
   return $http.post(baseUrl, newPoll);
 };
